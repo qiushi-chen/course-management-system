2efe54991907b058eeb41ab7830026899e4240c7
Q Chen <qche101@gmail.com>
2022-12-27 17:47:21 +1100

    apply layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Layout as AntdLayout } from 'antd';
+const { Content } = AntdLayout;
+
+import Layout from '@/components/layout';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Content>
+        <Component {...pageProps} />
+      </Content>
+    </Layout>
+  );
 }
