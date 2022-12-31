4b5bd2768b7cedb65e4dc03d969ee95994bc2a31
Q Chen <qche101@gmail.com>
2022-12-31 17:41:51 +1100

    update responsive css

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
-import { Button, Checkbox, Form, Input, Radio } from 'antd';
-import {
-  UserOutlined,
-  LockOutlined,
-  InfoCircleOutlined,
-} from '@ant-design/icons';
+import { Col, Row, Button, Checkbox, Form, Input, Radio } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 const Login: React.FC = () => {
   const onFinish = (values: any) => {
@@ -20,82 +16,87 @@ const Login: React.FC = () => {
 
   return (
     <>
-      <h1 style={{ textAlign: 'center' }}>Login to your account</h1>
+      <Row>
+        <Col
+          xs={{ span: 22, offset: 1 }}
+          sm={{ span: 18, offset: 3 }}
+          md={{ span: 14, offset: 5 }}
+          lg={{ span: 8, offset: 8 }}>
+          <h1 style={{ textAlign: 'center' }}>Login to your account</h1>
 
-      <Form
-        form={form}
-        layout='vertical'
-        style={{ margin: '50px 500px 20px 500px' }}
-        initialValues={{ remember: false }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete='off'>
-        <Form.Item
-          name='role'
-          rules={[{ required: true, message: 'Please select your role!' }]}>
-          <Radio.Group>
-            <Radio.Button value='student'>Student</Radio.Button>
-            <Radio.Button value='teacher'>Teacher</Radio.Button>
-            <Radio.Button value='manager'>Manager</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
+          <Form
+            form={form}
+            layout='vertical'
+            initialValues={{ remember: false }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete='off'>
+            <Form.Item
+              name='role'
+              rules={[{ required: true, message: 'Please select your role!' }]}>
+              <Radio.Group>
+                <Radio.Button value='student'>Student</Radio.Button>
+                <Radio.Button value='teacher'>Teacher</Radio.Button>
+                <Radio.Button value='manager'>Manager</Radio.Button>
+              </Radio.Group>
+            </Form.Item>
 
-        <Form.Item
-          name='email'
-          hasFeedback
-          tooltip='Email is a required field'
-          rules={[
-            {
-              required: true,
-              message: 'Please enter your email',
-            },
-            {
-              type: 'email',
-              message:
-                'The input is not valid E-mail! e.g. yourname@example.com',
-            },
-          ]}>
-          <Input
-            size='large'
-            placeholder='Please enter email'
-            prefix={<UserOutlined />}
-          />
-        </Form.Item>
+            <Form.Item
+              name='email'
+              hasFeedback
+              tooltip='Email is a required field'
+              rules={[
+                {
+                  required: true,
+                  message: 'Please enter your email',
+                },
+                {
+                  type: 'email',
+                  message:
+                    'The input is not valid E-mail! e.g. yourname@example.com',
+                },
+              ]}>
+              <Input
+                size='large'
+                placeholder='Please enter email'
+                prefix={<UserOutlined />}
+              />
+            </Form.Item>
 
-        <Form.Item
-          name='password'
-          hasFeedback
-          tooltip={{
-            title: 'Password is a required field',
-            icon: <InfoCircleOutlined />,
-          }}
-          rules={[{ required: true, message: 'Please enter your password!' }]}>
-          <Input.Password
-            size='large'
-            placeholder='Please enter password'
-            prefix={<LockOutlined />}
-          />
-        </Form.Item>
+            <Form.Item
+              name='password'
+              hasFeedback
+              rules={[
+                { required: true, message: 'Please enter your password!' },
+              ]}>
+              <Input.Password
+                size='large'
+                placeholder='Please enter password'
+                prefix={<LockOutlined />}
+              />
+            </Form.Item>
 
-        <Form.Item name='remember' valuePropName='unchecked'>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
+            <Form.Item name='remember' valuePropName='unchecked'>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
 
-        <Form.Item>
-          <Button
-            type='primary'
-            htmlType='submit'
-            className='login-form-button'
-            block>
-            Sign In
-          </Button>
-        </Form.Item>
+            <Form.Item>
+              <Button
+                type='primary'
+                htmlType='submit'
+                className='login-form-button'
+                block>
+                Sign In
+              </Button>
+            </Form.Item>
 
-        <Form.Item>
-          No Account?
-          <Link href='/signup'> Register now!</Link>
-        </Form.Item>
-      </Form>
+            <Form.Item>
+              No Account?
+              <Link href='/signup'> Register now!</Link>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
     </>
   );
 };
